621ae82b198a040d61831c2a7d40bf04a5f9feca
Pedro Oliveira <letter.pedro@gmail.com>
2022-10-25 21:08:22 -0300

    finalizada primeira questão

--- src/Application/App.java
+++ src/Application/App.java
@@ -1,5 +1,6 @@
 package Application;
 
+import java.math.BigInteger;
 import java.util.Scanner;
 
 public class App {
@@ -7,33 +8,48 @@ public class App {
         System.out.println("Digite o endereço de ip no formato: (255.255.255.255 / 32)");
         Scanner input = new Scanner(System.in);
         String address = input.nextLine();
+        System.out.println();
 
         String[] separatedAddress = address.split("/");
 
-        if (separatedAddress.length != 2)
+        if (separatedAddress.length != 2 || separatedAddress[0].split("\\.").length != 4)
             throw new Exception("Endereço digitado inválido");
 
-        Integer mascara = Integer.parseInt(separatedAddress[1].trim());
+        Integer mask = Integer.parseInt(separatedAddress[1].trim());
         String ip = separatedAddress[0].trim();
-        byte[] ipHex = ip.getBytes();
-        Long ipLong = ConvertToLong(ipHex); //ver a possibilidade de usar inteiro em vez de long
-        Integer diffInBytes = 32 - mascara; //http://mauda.com.br/?p=1271
+        Integer diffInBits = 32 - mask;
+        Double hostNumber = Math.pow(2, diffInBits) - 1;
 
+        long ipLong = ipToLong(ip);
+        ipLong += Math.pow(2, diffInBits) - 1;
+
+        
+        System.out.println("IP Inicial:");
         System.out.println(ip);
-        System.out.println(mascara);
-        System.out.println(ipHex);
-        System.out.println(ipLong >> mascara);
-        System.out.println(diffInBytes);
+        System.out.println();
+        System.out.println("Numero de hosts");
+        System.out.println(hostNumber.intValue());
+        System.out.println();
+        System.out.println("IP Final:");
+        System.out.print((ipLong & 0xFF000000) >> 24);
+        System.out.print(".");
+        System.out.print((ipLong & 0xFF0000) >> 16);
+        System.out.print(".");
+        System.out.print((ipLong & 0xFF00) >> 8);
+        System.out.print(".");
+        System.out.print(ipLong & 0xFF);
     }
 
-    static long ConvertToLong(byte[] bytes)
-    {
-        long value = 0l;
- 
-        for (byte b : bytes) {
-            value = (value << 8) + (b & 255);
+    static long ipToLong(String ipAddress) {
+        String[] ipAddressInArray = ipAddress.split("\\.");
+    
+        long result = 0;
+        for (int i = 0; i < ipAddressInArray.length; i++) {
+            int power = 3 - i;
+            int ip = Integer.parseInt(ipAddressInArray[i]);
+            result += ip * Math.pow(256, power);
         }
- 
-        return value;
+    
+        return result;
     }
 }
